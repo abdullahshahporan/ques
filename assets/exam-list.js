88bad6c724bb6130731be7e1aa317feb1cc0c720
Abdullah Md. ShahPoran <abdullahshahporan@gmail.com>
2026-06-25 10:35:49 +0600

    update

--- assets/exam-list.js
+++ assets/exam-list.js
@@ -16,5 +16,17 @@ window.examPages = [
     description: "২৫টি MCQ | ২৫ মিনিট",
     file: "complex.html",
     subject: "Higher Math"
+  },
+  {
+    title: "উচ্চতর গণিত ১ম পত্র: বহুপদী ও দ্বিঘাত সমীকরণ",
+    description: "২৫টি MCQ | ২৫ মিনিট",
+    file: "polynomial.html",
+    subject: "Higher Math"
+  },
+  {
+    title: "উচ্চতর গণিত ১ম পত্র: বিপরীত ত্রিকোণমিতিক ফাংশন",
+    description: "২৫টি MCQ | ২৫ মিনিট",
+    file: "inver_trigon.html",
+    subject: "Higher Math"
   }
 ];
