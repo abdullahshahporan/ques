7bdcc3e210f5b8356b038f29ea0ba7469cc9e2ee
Abdullah Md. ShahPoran <abdullahshahporan@gmail.com>
2026-09-11 06:55:23 +0600

    Added Question

--- assets/exam-list.js
+++ assets/exam-list.js
@@ -1,5 +1,11 @@
 window.examPages = [
   {
+    title: "পদার্থবিজ্ঞান ১ম পত্র: ভেক্টর + উচ্চতর গণিত ১ম পত্র: অন্তরীকরণ",
+    description: "সম্মিলিত পরীক্ষা | ৪৯টি MCQ | ২৭ মিনিট",
+    file: "p1m9.html",
+    subject: "Physics + Higher Math"
+  },
+  {
     title: "উচ্চতর গণিত ১ম পত্র: যোগজীকরণ",
     description: "২৫টি MCQ | ২৫ মিনিট",
     file: "integration.html",
